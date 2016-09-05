0b6568495e79136b8e99a3cc7273d26c3a1224ba
A.Bond <alexey.bond.94.55@gmail.com>
2016-09-05 14:41:40 +0600

    Generate LibGDX application.

---
+++ desktop/src/com/github/alexeybond/spectrum_lost/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.alexeybond.spectrum_lost.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.alexeybond.spectrum_lost.SpectrumLostGdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpectrumLostGdx(), config);
+	}
+}
---
+++ html/src/com/github/alexeybond/spectrum_lost/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.github.alexeybond.spectrum_lost.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.github.alexeybond.spectrum_lost.SpectrumLostGdx;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SpectrumLostGdx();
+        }
+}
