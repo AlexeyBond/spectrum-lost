c6662792167387c6533e0f17523210c55264c3eb
A.Bond <alexey.bond.94.55@gmail.com>
2016-10-06 15:41:27 +0600

    Add more levels to C1.

--- core/src/com/github/alexeybond/spectrum_lost/levels/hardcoded/Chapter1.java
+++ core/src/com/github/alexeybond/spectrum_lost/levels/hardcoded/Chapter1.java
@@ -223,5 +223,51 @@ public class Chapter1 extends Source {
 
                 W(0,1), W(0,2), W(0,3)
         );
+        L(4,8,
+                O(0,2, "expector"),
+                O(2,6, "expector"),
+
+                O(1,2, "hmirror", UP_LF),
+                O(1,1, "mirror", UP),
+                O(2,1, "mirror", UP),
+                O(2,3, "emitter", DN),
+                O(2,2, "switch", UP),
+
+                O(1,5, "mirror", UP_RT),
+                O(1,6, "mirror", DN_RT),
+                O(2,5, "emitter", LF),
+
+                W(0,0), W(0,1), W(0,3), W(0,4), W(0,5), W(0,6), W(0,7),
+                W(3,0), W(3,1), W(3,2), W(3,3), W(3,4), W(3,5), W(3,6), W(3,7),
+                W(1,0), W(2,0), W(1,7), W(2,7),
+                W(2,4)
+        );
+        L(8,7,
+                O(1,2, "emitter", RT),
+                O(2,2, "switch", LF),
+                O(3,2, "hmirror", DN_LF),
+                O(2,1, "mirror", UP_RT),
+                O(3,1, "mirror", UP_LF),
+                O(3,4, "mirror", DN_LF),
+                O(4,4, "hmirror", UP_RT),
+                O(4,5, "hmirror", DN_RT),
+                O(5,5, "mirror", DN_LF),
+                O(5,4, "switch", RT),
+
+                O(5,1, "emitter", UP),
+                O(5,2, "mirror", DN_RT),
+
+                O(6,4, "emitter", LF),
+
+                O(2,4, "expector"),
+                O(2,5, "expector"),
+                O(6,2, "expector"),
+
+                W(5,3), W(6,3), W(2,3), W(1,3), W(0,3), W(7,3),
+                W(1,4), W(1,5), W(1,6), W(2,6), W(3,6), W(4,6), W(5,6), W(6,6), W(6,5), W(7,5), W(7,4),
+
+                W(0,2), W(0,1), W(1,1), W(1,0), W(2,0), W(3,0), W(4,0), W(4,1), W(5,0), W(6,0), W(6,1),
+                W(7,1), W(7,2)
+        );
     }
 }
