0084402ecf048f4425add2cb06feca944ae24746
A.Bond <alexey.bond.94.55@gmail.com>
2016-10-08 19:03:00 +0600

    Move all levels to JSON. Delete all hardcode.

--- core/src/com/github/alexeybond/spectrum_lost/SpectrumLostGdx.java
+++ core/src/com/github/alexeybond/spectrum_lost/SpectrumLostGdx.java
@@ -4,10 +4,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.github.alexeybond.spectrum_lost.cell_types.$CellTypes;
-import com.github.alexeybond.spectrum_lost.levels.hardcoded.Chapter0;
-import com.github.alexeybond.spectrum_lost.levels.hardcoded.Chapter1;
-import com.github.alexeybond.spectrum_lost.levels.hardcoded.Chapter3;
-import com.github.alexeybond.spectrum_lost.levels.hardcoded.Chapter4;
 import com.github.alexeybond.spectrum_lost.levels.json.JsonSource;
 import com.github.alexeybond.spectrum_lost.screens.$Screen;
 import com.github.alexeybond.spectrum_lost.screens.GameScreen;
@@ -23,8 +19,7 @@ public class SpectrumLostGdx extends ApplicationAdapter {
 		$Sprite2DViews.register();
 
 		currentScreen = new GameScreen(
-//				new Chapter0()/*new Chapter1()*//*new Chapter3()*//*new Chapter4()*/
-				new JsonSource("levels/chapter0.json")
+				new JsonSource("levels/chapter3.json")
 				);
 		currentScreen.show(null);
 		currentScreen.unpause();
